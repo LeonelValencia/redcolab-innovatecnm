181b0adcaceb011ca8e41e8624b42a86feb04330
Galarcon128 <alarcongabriel128@gmail.com>
2023-10-08 20:51:28 -0600

    fix flip bug on init test

--- src/components/userManager/singUp/testForm/index.jsx
+++ src/components/userManager/singUp/testForm/index.jsx
@@ -9,12 +9,29 @@ import "./testForm.css";
 import { CSSTransition } from "react-transition-group";
 
 export default function TestForm({ name = "Test", info, agents, setValues }) {
-  const [flip, setFlip] = useState(false);
+  const [cardFront, setCardFront] = useState(true);
+  const [cardBack, setCardBack] = useState(false);
   const [accept, setAccept] = useState(false);
   const nodeRef = useRef(null);
+  const nodeRef2 = useRef(null);
+
 
   const handleFinishTest = (testValues, isSkip) => {
     setValues(testValues, isSkip);
+  };
+
+  const handleFlip = () => {
+    if (cardFront) {
+      setCardFront(false);
+      setTimeout(() => {
+        setCardBack(true);
+      }, 500);
+    } else {
+      setCardBack(false);
+      setTimeout(() => {
+        setCardFront(true);
+      }, 500);
+    }
   };
   /*
   if (accept) {
@@ -62,13 +79,7 @@ export default function TestForm({ name = "Test", info, agents, setValues }) {
           background: "#9ADBF9",
         }}
       >
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => {
-            setFlip(!flip);
-          }}
-        >
+        <Button variant="contained" color="success" onClick={handleFlip}>
           Continuar
         </Button>
       </div>
@@ -77,11 +88,6 @@ export default function TestForm({ name = "Test", info, agents, setValues }) {
 
   const back = (
     <div>
-      <div>
-        <Typography gutterBottom variant="h6" component="div">
-          {info.name}
-        </Typography>
-      </div>
       <div
         style={{
           margin: 1,
@@ -115,13 +121,7 @@ export default function TestForm({ name = "Test", info, agents, setValues }) {
         }}
       >
         <div style={{ display: "flex" }}>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={() => {
-              setFlip(!flip);
-            }}
-          >
+          <Button variant="contained" color="secondary" onClick={handleFlip}>
             {"<="}
           </Button>
           <Button
@@ -165,45 +165,33 @@ export default function TestForm({ name = "Test", info, agents, setValues }) {
       <Typography component="h1" variant="h5">
         {name}
       </Typography>
-      <buttom />
       <div>
-        <CSSTransition
-        in={flip}
-        nodeRef={nodeRef}
-        timeout={300}
-        classNames="cardBack"
-        unmountOnExit
-        >
-          <div ref={nodeRef} >
-            <Card  content={back} />
-          </div>
-        </CSSTransition>
-
+        <div style={{ width: "350px", height: "560px" }}>
+          <CSSTransition
+            in={cardFront}
+            nodeRef={nodeRef}
+            timeout={500}
+            classNames="flip"
+            unmountOnExit
+          >
+            <div ref={nodeRef} className="testCard">
+              <Card content={front} />
+            </div>
+          </CSSTransition>
+          <CSSTransition
+            in={cardBack}
+            nodeRef={nodeRef2}
+            timeout={500}
+            classNames="flip"
+            unmountOnExit
+          >
+            <div ref={nodeRef2} className="testCard">
+              <Card content={back} />
+            </div>
+          </CSSTransition>
+        </div>
       </div>
     </Box>
   );
 }
 
-/*
-        <CSSTransition
-        in={!flip}
-        nodeRef={nodeRef}
-        timeout={300}
-        classNames="cardFront"
-        unmountOnExit
-        >
-          <div ref={nodeRef} >
-            <Card  content={front} />
-          </div>
-        </CSSTransition>
-
-
-      <div style={{ width: "350px", height: "560px" }}>
-        <ReactFlipCard
-          flipByProp={flip}
-          flipTrigger={"disabled"}
-          frontComponent={}
-          backComponent={}
-        />
-      </div>
-*/
