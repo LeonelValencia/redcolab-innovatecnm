9dacd94eb2f8af26f767b7ca31ff118925bce844
Galarcon128 <alarcongabriel128@gmail.com>
2023-10-23 11:16:01 -0600

    add radar grafic

--- src/apps/user/sections/skills/trendInterest.jsx
+++ src/apps/user/sections/skills/trendInterest.jsx
@@ -6,6 +6,7 @@ import Grid from "@mui/material/Grid";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import DataVerifier from "../../../../components/webServices/tools";
 import {
     Chart as ChartJS,
     RadialLinearScale,
@@ -26,47 +27,14 @@ import {
     Legend
   );
 
-  export const data = {
-    options: {
-      plugins: {
-        title: {
-            display: false,
-            text: 'Custom Chart Title'
-        }
-    },
-      scales: {
-        r: {
-          pointLabels: {
-            color: "white",
-            font: {
-              size: 14,
-            },
-          },
-          angleLines: {
-            color: "white",
-          },
-          ticks: {
-            // display:false,
-            color: "#000",
-            stepSize: 1,
-          },
-          grid: {
-            color: "#fff",
-            lineWidth: 2,            
-  
-          },
-          beginAtZero: true,
-        },
-      },
-    },
-  };
+
 
 const BarChart = ({ data }) => {
     const chartData = {
       labels: data.map(item => item.label),
       datasets: [
         {
-          label: "Ventas por mes",
+          label: "Tendencia de interés",
           data: data.map(item => item.value),
           backgroundColor: "rgba(75, 192, 192, 0.2)",
           borderColor: "rgba(75, 192, 192, 1)",
@@ -74,10 +42,24 @@ const BarChart = ({ data }) => {
         },
       ],
     };
+
+    const options = {
+        tension: 1,
+        pointRadius: 15,
+        scales: {
+            r: {
+                angleLines: {
+                    display: false
+                },
+                suggestedMin: 10,
+                suggestedMax: 100
+            }
+        }
+    }
   
     return (
       <div>
-         <Radar data={skills} options={data.options} />;
+         <Radar data={chartData} options={options} />
       </div>
     );
   };
@@ -87,11 +69,14 @@ export default function TrendInterest({ user }) {
   const handleDrawer = () => {
     setOpen(!open);
   };
+  if(!DataVerifier.isValidArray(user.skills.trendInterest)){
+    return null
+  }
+  const trendInterest = user.skills.trendInterest[0]
   const data = [
-    { label: "Formal", value: 10 },
-    { label: "Naturaleza", value: 15 },
-    { label: "Sociales", value: 30 },
-    // ... otros meses
+    { label: "Fácticas", value: trendInterest.formal*100/250 },
+    { label: "Naturaleza", value: trendInterest.nature*100/250 },
+    { label: "Sociales", value: trendInterest.social*100/250 },
   ];
   return (
     <Grid item key={"card01"} xs={12} sm={6} md={4}>
@@ -101,7 +86,6 @@ export default function TrendInterest({ user }) {
             Tendencia de Interés
           </Typography>
           <div>
-      <h1>Mi Gráfico de Barras</h1>
       <BarChart data={data}/>
     </div>
           <Typography>
@@ -115,13 +99,10 @@ export default function TrendInterest({ user }) {
           </Typography>
         </CardContent>
         <CardActions>
-          <Button onClick={handleDrawer}>Ver Resumen</Button>
+          <Button onClick={handleDrawer} color="success" >¿Como mejorar?</Button>
           <Drawer anchor={"bottom"} open={open} onClose={handleDrawer}>
             <Summary trendInterest={user.skills.trendInterest} />
           </Drawer>
-          <Button size="small" color="success">
-            ¿Como mejorar?
-          </Button>
         </CardActions>
       </Card>
     </Grid>
@@ -132,7 +113,7 @@ function Summary({ trendInterest }) {
   
   return (
     <div>
-      <h1>Mi Gráfico de Barras</h1>
+        Hola
       {/*<BarChart data={data}/>
       */}
     </div>
